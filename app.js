237b7ee0561704be6a57bb41f413318ee13c18fa
Silviana <107526175+silviana-hs@users.noreply.github.com>
2022-08-17 17:13:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'asddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqy',
+  secret: 'ñasddfilhpaf78h78032h780g780fg780asg780dsbovncubuyvqy',
   cookie: {
     secure: false,
     maxAge: 99999999999
